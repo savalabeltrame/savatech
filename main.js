7be4bd19841b0fa0ee85e3c4184ebf30964e8efc
savalabeltrame <savalatech63@gmail.com>
2024-12-24 15:05:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+class MobileNavbar {
+    constructor() {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(this.navList);
+        this.navLinks = document.querySelectorAll(this.navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            console.log();
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = "navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s");
+        });
+    }
+    handleClick() {
+       
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks()
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this ;
+    }
+}
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+
+);
